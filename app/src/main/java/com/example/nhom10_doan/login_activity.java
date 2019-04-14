7a6ecd3268aa386718f2584a0faf3bbf0015d8cd
NgocZ <ngngngoc2101@gmail.com>
2019-04-15 06:57:23 +0700

    login + register

---
+++ app/src/main/java/com/example/nhom10_doan/login_activity.java
@@ -0,0 +1,25 @@
+package com.example.nhom10_doan;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class login_activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Button btndangky = (Button) findViewById(R.id.btndangky);
+        btndangky.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(login_activity.this, register_activity.class);
+                startActivity(intent);
+            }
+        });
+    }
+
+}
--- app/src/main/java/com/example/nhom10_doan/register_activity.java
+++ app/src/main/java/com/example/nhom10_doan/register_activity.java
@@ -3,11 +3,11 @@ package com.example.nhom10_doan;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+public class register_activity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_register);
     }
 }
